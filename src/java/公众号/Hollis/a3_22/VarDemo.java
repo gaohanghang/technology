76b9fc745fc252dcb0d01592ac0b646d1b4e671d
gaohanghang <1341947277@qq.com>
2018-03-22 10:26:24 +0800

    java10

---
+++ src/java/公众号/Hollis/a3_22/VarDemo.java
@@ -0,0 +1,28 @@
+package java.公众号.Hollis.a3_22;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class VarDemo
+{
+//    public static void main(String args[])
+//    {
+//        //初始化局部变量
+//        var string = "hollis";
+//        //初始化局部变量
+//        var stringList = new ArrayList<String>();
+//        stringList.add("hollis");
+//        stringList.add("chuang");
+//        stringList.add("weChat:hollis");
+//        stringList.add("blog:http://www.hollischuang.com");
+//        //增强for循环的索引
+//        for (var s : stringList){
+//            System.out.println(s);
+//        }
+//        //传统for循环的局部变量定义
+//        for (var i = 0; i < stringList.size(); i++){
+//            System.out.println(stringList.get(i));
+//        }
+//
+//    }
+}
