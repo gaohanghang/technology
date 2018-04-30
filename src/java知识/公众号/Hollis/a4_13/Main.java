8ed97b65b43aae211818a7debd9666183375dd20
gaohanghang <1341947277@qq.com>
2018-04-30 19:37:56 +0800

    学习

---
+++ src/java知识/公众号/Hollis/a4_13/Main.java
@@ -0,0 +1,7 @@
+package java知识.公众号.Hollis.a4_13;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/java知识/公众号/Hollis/a4_27/Main.java
@@ -0,0 +1,12 @@
+package java知识.公众号.Hollis.a4_27;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String,Boolean> map = new HashMap<>();
+        Boolean b = map!= null ? map.get("test") : false;
+        System.out.println(b);
+    }
+}
