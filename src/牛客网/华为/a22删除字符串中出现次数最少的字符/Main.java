84f9b840f6f7a8bef8734718086c4f203373b7a0
gaohanghang <1341947277@qq.com>
2018-04-30 12:37:30 +0800

    学习

---
+++ src/牛客网/华为/a22删除字符串中出现次数最少的字符/Main.java
@@ -0,0 +1,35 @@
+package 牛客网.华为.a22删除字符串中出现次数最少的字符;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+
+            String string = sc.nextLine();
+            char[] A = string.toCharArray();
+            Map<Character,Integer> m = new LinkedHashMap<>();
+
+
+            for (char c:A){
+                if (!m.containsKey(c)){
+                    m.put(c,1);
+                }else {
+                    m.put(c,m.get(c)+1);
+                }
+            }
+            //获取最小的次数
+            Collection<Integer> al = m.values();
+            int index = Collections.min(al);
+            StringBuffer sb = new StringBuffer();
+            //将出现次数不是最小次数的放进StringBuffer中
+            for (char c : A) {
+                if (m.get(c)!=index)
+                    sb.append(c);
+            }
+
+            System.out.println(sb.toString());
+        }
+    }
+}
