d3c71b88d64c34523a7ea2f52363a0bd54a8ba07
gaohanghang <1341947277@qq.com>
2018-06-18 08:01:19 +0800

    异常的相关知识

---
+++ src/java知识/Java基础/异常/自定义异常/Java自定义异常_优雅的处理异常/OrderExceptionEnum.java
@@ -0,0 +1,9 @@
+package java知识.Java基础.异常.自定义异常.Java自定义异常_优雅的处理异常;
+
+/**
+ * 第一错误吗枚举；规范错误集合
+ * @author GaoHangHang
+ * @date 2018/06/17 21:54
+ **/
+public class OrderExceptionEnum {
+}
---
+++ src/java知识/Java基础/异常/自定义异常/demoException.java
@@ -0,0 +1,15 @@
+package java知识.Java基础.异常.自定义异常;
+
+/**
+ * @author GaoHangHang
+ * @date 2018/06/17 21:49
+ **/
+public class demoException extends Exception { //自定义异常类继承于Exception类
+    public demoException() { //无参构造方法
+
+    }
+
+    public demoException(String me){ //有参构造方法
+        super(me);//调用父类构造方法并传参数进去
+    }
+}
