2ec8127ec6e942753183e2db642bc8f45c8ae4e9
“gaohanghang” <1341947277@qq.com>
2018-09-08 17:26:57 +0800

    更新

--- src/java知识/Java基础/泛型/通配符/testWildCards.java
+++ src/java知识/Java基础/泛型/通配符/testWildCards.java
@@ -1,4 +1,18 @@
 package java知识.Java基础.泛型.通配符;
 
+import java.util.Collection;
+
 public class testWildCards {
+
+    public void testWildCards(Collection<?> collection) {
+
+        //collection.add(123);
+        //collection.add("hello");
+        //collection.add(new Object());
+
+        collection.iterator().next();
+        collection.size();
+
+    }
+
 }
