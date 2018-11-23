8ac048c4d6db4da303af715d5b0e97a650ff3bd2
gaohanghang <1341947277@qq.com>
2018-11-23 13:45:26 +0800

    :bento: Java学习

---
+++ src/java知识/java核心技术/a3_5_2/Test.java
@@ -0,0 +1,12 @@
+package java知识.java核心技术.a3_5_2;
+
+/**
+ * 数值类型之间的转换
+ */
+public class Test {
+    public static void main(String[] args) {
+        int n = 123456789;
+        float f = n; // f is 1.23456792E8
+        System.out.println(f);
+    }
+}
