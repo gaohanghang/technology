486d16e9043d5cd28155f669260fdb5fdf0867c3
gaohanghang <1341947277@qq.com>
2018-12-06 09:03:27 +0800

    java核心技术学习 数组

---
+++ src/java知识/java核心技术/a3/a3_10/a3_10_6/Test.java
@@ -0,0 +1,12 @@
+package java知识.java核心技术.a3.a3_10.a3_10_6;
+
+/**
+ * @Description: 多维数组
+ * @author: Gao Hang Hang
+ * @date 2018/12/06 8:59 AM
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
