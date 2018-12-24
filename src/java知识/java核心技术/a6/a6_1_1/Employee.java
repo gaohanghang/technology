2dbdcdafe83f70c0741846122677d39d152422dd
gaohanghang <1341947277@qq.com>
2018-12-24 17:57:40 +0800

    java核心技术学习 对象克隆

--- src/java知识/java核心技术/a6/a6_1_1/Employee.java
+++ src/java知识/java核心技术/a6/a6_1_1/Employee.java
@@ -1,13 +1,16 @@
 package java知识.java核心技术.a6.a6_1_1;
 
+import java.util.Date;
+
 /**
  * @Description:
  * @author: Gao Hang Hang
  * @date 2018/12/24 14:06
  */
-public class Employee implements Comparable<Employee>{
+public class Employee implements Comparable<Employee>, Cloneable{
     private String name;
     private double salary;
+    private Date hireDay;
 
     public Employee(String name, double salary) {
         this.name = name;
@@ -27,6 +30,10 @@ public class Employee implements Comparable<Employee>{
         salary += raise;
     }
 
+    public void setHireDay(int year, int month, int day) {
+        this.hireDay = new Date(year, month, day);
+    }
+
     /**
      * Compares employees by salary
      * @param other another Employee object
@@ -36,5 +43,22 @@ public class Employee implements Comparable<Employee>{
     @Override
     public int compareTo(Employee other) {
         return Double.compare(salary, other.salary);
+    }
+
+    /*浅拷贝*/
+    // raise visibility level to public, change return type
+    // public Employee clone() throws CloneNotSupportedException {
+    //     return (Employee) super.clone();
+    // }
+
+    /*深拷贝*/
+    public Employee clone() throws CloneNotSupportedException {
+        // call Object.clone()
+        Employee cloned = (Employee) super.clone();
+
+        // clone mutable fields
+        cloned.hireDay = (Date) hireDay.clone();
+
+        return cloned;
     }
 }
