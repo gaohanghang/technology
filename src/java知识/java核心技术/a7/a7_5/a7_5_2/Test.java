69d66341f4ad6f2fbf8294bcc3be3649c2d3ce85
gaohanghang <1341947277@qq.com>
2019-01-02 11:16:53 +0800

    🍖 java核心技术学习

---
+++ src/java知识/java核心技术/a7/a7_5/a7_5_2/Test.java
@@ -0,0 +1,17 @@
+package java知识.java核心技术.a7.a7_5.a7_5_2;
+
+import java.util.logging.Logger;
+
+/**
+ * @Description:
+ * @author: Gao Hang Hang
+ * @date 2019/01/02 10:56
+ */
+public class Test {
+    private static final Logger myLogger = Logger.getLogger("com.mycompany.myapp");
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/java知识/java核心技术/a8/a8_1/Test.java
@@ -0,0 +1,14 @@
+package java知识.java核心技术.a8.a8_1;
+
+/**
+ * @Description:
+ * @author: Gao Hang Hang
+ * @date 2019/01/02 11:06
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+
+
+}
