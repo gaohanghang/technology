f2adb3110c1b8869b3d123b96ba5ddde9a574e65
gaohanghang <1341947277@qq.com>
2019-07-20 14:13:02 +0800

    <添加>(添加): 代码添加

---
+++ src/java知识/java核心技术/a9/a9_7/ShuffleTest.java
@@ -0,0 +1,25 @@
+package java知识.java核心技术.a9.a9_7;
+
+import java知识.java核心技术.a9.a9_1.a9_1_2.Collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author Gao Hang Hang
+ * @Date 2019-07-20 13:41
+ **/
+public class ShuffleTest {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 49; i++)
+            numbers.add(i);
+        Collections.shuffle(numbers);
+        List<Integer> winningCombination = numbers.subList(0, 6);
+        Collections.sort(winningCombination);
+        System.out.println(winningCombination);
+    }
+}
---
+++ src/java知识/公众号/Java技术栈/a2019/Test.java
@@ -0,0 +1,12 @@
+package java知识.公众号.Java技术栈.a2019;
+
+/**
+ * @Description
+ * @Author Gao Hang Hang
+ * @Date 2019-07-19 02:12
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("x".getBytes().length);
+    }
+}
