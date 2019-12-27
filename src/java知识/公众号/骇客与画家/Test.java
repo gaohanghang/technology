e4de10aa330879392031a5e79c5a8d54ce4b2c6b
gaohanghang <1341947277@qq.com>
2019-12-27 09:17:17 +0800

    <添加>(添加): 代码添加

---
+++ src/java知识/公众号/骇客与画家/Test.java
@@ -0,0 +1,18 @@
+package java知识.公众号.骇客与画家;
+
+/**
+ * @Description
+ * @Author Gao Hang Hang
+ * @Date 2019-12-26 21:55
+ **/
+public class Test {
+
+    public synchronized void method1(){
+        // do something
+    }
+
+    public synchronized void method2(){
+        // do something
+    }
+
+}
