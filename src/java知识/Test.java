be17bfd4cc6012e292cd85065ee99af2ab9a8585
gaohanghang <1341947277@qq.com>
2020-03-11 09:12:53 +0800

    <添加>(添加): 代码添加

--- src/java知识/Test.java
+++ src/java知识/Test.java
@@ -1,7 +1,12 @@
 package java知识;
 
 public class Test {
+    // 分页
     public static void main(String[] args) {
-
+        Integer totalCount = 10;
+        Integer pageSize = 10;
+        // (10 + 9 ) / 10
+        Integer totalPage = (totalCount + pageSize - 1) / pageSize;
+        System.out.println(totalPage);
     }
 }
